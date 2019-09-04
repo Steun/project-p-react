14b031882ad9e09cb9dc3b0516021f954f4a3137
Teun Kelting <teun@tjuna.com>
2019-09-04 17:10:44 +0200

    Project --> index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,91 +1,55 @@
-import React from 'react';
-import Link from 'next/link';
-import Head from 'next/head';
-import Nav from '../components/nav';
+import React, {useState} from 'react'
+import css from './_project.scss'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-    </Head>
+import Sidebar from '../components/sidebar/sidebar'
+import Occupancy from '../components/statistics/occupancy/Occupancy'
+import Visits from '../components/visits/Visits';
 
-    <Nav />
+export interface Props {
+    test2: string
+}
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+export interface State {
+    occupied: boolean,
+}
 
-      <div className='row'>
-        <Link href='https://github.com/zeit/next.js#setup'>
-          <a className='card'>
-            <h3>Getting Started &rarr;</h3>
-            <p>Learn more about Next.js on GitHub and in their examples.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-          <a className='card'>
-            <h3>Examples &rarr;</h3>
-            <p>Find other example boilerplates on the Next.js GitHub.</p>
-          </a>
-        </Link>
-        <Link href='https://github.com/zeit/next.js'>
-          <a className='card'>
-            <h3>Create Next App &rarr;</h3>
-            <p>Was this tool helpful? Let us know how we can improve it!</p>
-          </a>
-        </Link>
-      </div>
-    </div>
+class Home extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = {occupied: false};
+    }
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+    handleOccupancyClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        this.setState(({occupied}) => ({
+            occupied: !occupied
+        }));
+    };
+
+    componentDidMount() {
+        // Fetch location data and update state
+        fetch('https://project-p.vps101.tjuna.com/locations/1/')
+            .then(res => res.json())
+            .then(res => this.setState(Object.assign({}, this.state, res.data)))
+    }
+
+    render() {
+        const {occupied} = this.state;
+
+        return (
+            <div className={css.wrapper}>
+                <div className={css.project}>
+                    <Sidebar/>
+
+                    <main className={css.content}>
+                        <Visits/>
+                        <Occupancy occupied={occupied}/>
+                        <button type="button" onClick={this.handleOccupancyClick}>Toggle occupancy</button>
+                    </main>
+
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Home
