f6902e04827df0ab104a7378d6d5bfdfe935f828
Teun Kelting <teun@tjuna.com>
2019-09-05 17:33:58 +0200

    Added favicons

--- layouts/meta.tsx
+++ layouts/meta.tsx
@@ -5,8 +5,10 @@ const Meta = () => (
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-      <title>Project P</title>
+      <meta charSet="utf-8" />      <title>Project P</title>
+      <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
+       <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
       <link
         href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap"
         rel="stylesheet"
