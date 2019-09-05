153d7a8ff185a7c7ec479559da72bb2a7c5b74d5
Teun Kelting <teun@tjuna.com>
2019-09-05 16:16:45 +0200

    Added OccupancyOverlay

---
+++ components/occupancy-overlay/OccupancyOverlay.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import css from './OccupancyOverlay.scss';
+
+export interface Props {
+    occupied: boolean;
+}
+
+export const OccupancyOverlay: FC<Props> = props => {
+    const { occupied } = props;
+
+    return (
+        <div className={css.overlay + ' ' + (occupied ? css.overlay__active : null)}/>
+    )
+
+};
+
+export default OccupancyOverlay;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Meta from '../layouts/meta';
 import Sidebar from '../components/sidebar/sidebar';
 import Occupancy from '../components/statistics/occupancy/Occupancy';
 import Visits from '../components/visits/Visits';
+import OccupancyOverlay from "../components/occupancy-overlay/OccupancyOverlay";
 
 export interface Props {
   occupied: boolean;
@@ -28,62 +29,24 @@ export const Home = () => {
   }
 
   return (
-      <>
-        <Meta />
-        <div className={css.wrapper}>
-          <div className={css.project}>
-            <Sidebar />
+    <>
+      <Meta />
+      <div className={css.wrapper}>
+        <OccupancyOverlay occupied={occupied}/>
+        <div className={css.project}>
+          <Sidebar />
 
-            <main className={css.content}>
-              <Occupancy occupied={occupied} />
-              <button type="button" onClick={handleOccupancyClick}>
-                Toggle occupancy
-              </button>
-              <Visits />
-            </main>
-          </div>
+          <main className={css.content}>
+            <Occupancy occupied={occupied} />
+            <button type="button" onClick={handleOccupancyClick}>
+              Toggle occupancy
+            </button>
+            <Visits />
+          </main>
         </div>
-      </>
+      </div>
+    </>
   );
 };
-
-// class Home extends React.Component<Props, State> {
-//     constructor(props) {
-//         super(props);
-//         this.state = {occupied: false};
-//     }
-//
-//     handleOccupancyClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-//         this.setState(({occupied}) => ({
-//             occupied: !occupied
-//         }));
-//     };
-//
-//     componentDidMount() {
-//         // Fetch location data and update state
-//         fetch('https://project-p.vps101.tjuna.com/locations/1/')
-//             .then(res => res.json())
-//             .then(res => this.setState(Object.assign({}, this.state, res.data)))
-//     }
-//
-//     render() {
-//         const {occupied} = this.state;
-//
-//         return (
-//             <div className={css.wrapper}>
-//                 <div className={css.project}>
-//                     <Sidebar/>
-//
-//                     <main className={css.content}>
-//                         <Visits/>
-//                         <Occupancy occupied={occupied}/>
-//                         <button type="button" onClick={this.handleOccupancyClick}>Toggle occupancy</button>
-//                     </main>
-//
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
 
 export default Home;
