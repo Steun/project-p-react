b810abd4bf0e3aecd89873cf2ad5926076a01d06
Teun Kelting <teun@tjuna.com>
2019-09-05 17:34:09 +0200

    Part of index test written

--- pages/index.spec.tsx
+++ pages/index.spec.tsx
@@ -5,11 +5,9 @@ import { render } from '@testing-library/react';
 import Home from './index';
 
 describe('index', () => {
+
   it('should render', () => {
-    const mockComponent = () => <p>Test app</p>;
-
-    const { getByText } = render(<Home Component={mockComponent} />);
-
-    expect(getByText('Test app')).toBeInTheDocument();
+    const { getByText } = render(<Home />);
+    expect(getByText('Toggle occupancy')).toBeInTheDocument();
   });
 });
