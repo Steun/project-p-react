213d6f2d75cfe7511ff0a01cafde6815e0405e5d
Teun Kelting <teunkelting@hotmail.com>
2019-09-24 21:36:17 +0200

    Small style changes

--- layouts/meta.tsx
+++ layouts/meta.tsx
@@ -5,7 +5,9 @@ const Meta = () => (
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />      <title>Project P</title>
+      <meta charSet="utf-8" />      
+      <title>Project P</title>
+      <meta name="theme-color" content="#84d4ff"></meta>
       <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
       <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"/>
